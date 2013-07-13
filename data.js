2323f907a591a1b5bc833e14a84add0a43602aa5
tymon <tymanski@gmail.com>
2013-07-13 19:15:09 +0700

    First version

---
+++ data.js
@@ -0,0 +1,25 @@
+/**
+ * Config file
+ */
+
+// Base of URL path
+var linkBase = "http://localhost/branches"
+
+// Data source
+var data = [
+	{
+		name		: "bob marley",
+		path		: "/pages/bob/marley",
+		keywords	: ""
+	},
+	{
+		name		: "Bob dylan",
+		path		: "other/websites/dylan",
+		keywords	: "show"
+	},
+	{
+		name		: "backstreet boys",
+		path		: "/other/musics/bb",
+		keywords	: "red, blue"
+	}
+]
---
+++ script/data.js
@@ -0,0 +1,17 @@
+var data = [
+	{
+		name		: "Animal Heatlh Poland",
+		path		: "www.wp.pl",
+		keywords	: "AH Poland, AH, "
+	},
+	{
+		name		: "Bayer Premia",
+		path		: "bgb/BayerHealth/Premia",
+		keywords	: "Premia"
+	},
+	{
+		name		: "Yazflex",
+		path		: "/CC/Bayer/Yazflex/Australia",
+		keywords	: "myperiods, yazflex"
+	},
+]

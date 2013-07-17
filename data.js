56998f951c90ba974223e404a7554dab9ec506f4
tymon <tymanski@gmail.com>
2013-07-18 00:08:17 +0700

    poprawki

--- data.js
+++ data.js
@@ -3,32 +3,400 @@
  */
 
 // Base of URL path
-var linkBase = "http://localhost/branches"
+var linkBase = "https://teamsite.bayerbbs-hosting.com/iw-cc/command/iw.ui?vpath=";
 
 // Data source
 var data = [
-	{
-		name		: "bob marley",
-		path		: "/pages/bob/marley",
-		keywords	: "",
+    {
+        name        : "Jaydess Switzerland",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Jaydess/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
         wwwDev      : "",
         wwwTest     : "",
         wwwProd     : ""
-	},
-	{
-		name		: "Bob dylan",
-		path		: "other/websites/dylan",
-		keywords	: "show",
+    },
+    {
+        name        : "Priorin Switzerland",
+        path        : "//bybz25/default/main/MarketingSites/CC/Priorin/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
         wwwDev      : "",
         wwwTest     : "",
         wwwProd     : ""
-	},
-	{
-		name		: "backstreet boys",
-		path		: "/other/musics/bb",
-		keywords	: "red, blue",
+    },
+    {
+        name        : "Animal Health Hungary ",
+        path        : "//bybz25/default/main/MarketingSites/AH/AnimalHealthProducts/Hungary/Partnerprogram/WORKAREA/htdocs",
+        keywords    : "AH",
         wwwDev      : "",
         wwwTest     : "",
         wwwProd     : ""
-	}
-]
+    },
+    {
+        name        : "Yazflex Australia",
+        path        : "//bybz25/default/main/MarketingSites/BSP/YazFlex/Australia/MyPeriods/WORKAREA/htdocs",
+        keywords    : "MyPeriods",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Bayer Premia Mexico",
+        path        : "//bybz25/default/main/MarketingSites/AH/AnimalHealthProducts/Mexico/Premia/WORKAREA/htdocs",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence Poland",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/Poland/WORKAREA/htdocs",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence Romania",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/Romania/WORKAREA/htdocs",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Animal Health Poland",
+        path        : "//bybz25/default/main/MarketingSites/AH/AnimalHealthProducts/Poland/WORKAREA/htdocs",
+        keywords    : "",
+        wwwDev      : "http://www.onet.pl",
+        wwwTest     : "http://www.wp.pl",
+        wwwProd     : "http://www.interia.pl"
+    },
+    {
+        name        : "Xarelto",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Xarelto/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Pijnstiller infocentrum",
+        path        : "//bybz25/default/main/MarketingSites/CC/Aspirin/Netherlands/pijnstillerinfocentrum/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "AMD Diagnose",
+        path        : "//bybz25/default/main/MarketingSites/BSP/EYLEA/Switzerland/AMDDiagnose/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Advantix",
+        path        : "//bybz25/default/main/MarketingSites/AH/Advantix/Hungary/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Antrapro.ch",
+        path        : "//bybz25/default/main/MarketingSites/CC/Losec-Antra/Switzerland/AntraPro/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "BAYER CC AG",
+        path        : "//bybz25/default/main/CorpSites/BHC/CC/Basel/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "BayerFarmHU",
+        path        : "//bybz25/default/main/MarketingSites/AH/Farming/Hungary/WORKAREA/htdocs/",
+        keywords    : "Keywords",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Bayovac",
+        path        : "//bybz25/default/main/MarketingSites/AH/VetsPortal/Mexico/Premia/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Berocca - Main",
+        path        : "//bybz25/default/main/MarketingSites/CC/Berocca/Australia/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Berrocca 28 days",
+        path        : "//bybz25/default/main/MarketingSites/CC/Berocca/Australia/BeroccaPromo/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Canesten AU",
+        path        : "//bybz25/default/main/MarketingSites/CC/Canesten/Australia/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Canesten NZ",
+        path        : "//bybz25/default/main/MarketingSites/CC/Canesten/NewZealand/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Canesten Valentine",
+        path        : "//bybz25/default/main/MiscSites/BHC/Campaigns/Promos/Australia/WomensConfidential/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Dermacalm.ch",
+        path        : "//bybz25/default/main/MarketingSites/CC/Dermacalm/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Drontal",
+        path        : "//bybz25/default/main/MarketingSites/AH/Drontal/Hungary/WORKAREA/htdocs/",
+        keywords    : "Keywords",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Eylea",
+        path        : "//bybz25/default/main/MarketingSites/BSP/EYLEA/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Fellelo",
+        path        : "//bybz25/default/main/MarketingSites/AH/AnimalHealthProducts/Hungary/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Flexyess Awareness",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Flexyess/International/WORKAREA/htdocs/",
+        keywords    : "flexyess.com",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Flexyless PeriodManagement",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Flexyess/PeriodManagement/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Flexyless Self-Detail",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Flexyess/FlexyessDetail/International/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence BULGARIAN",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/Bulgaria/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence CZECH",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/CzechRepublic/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence ENGLISH",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence FRANCE",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/France/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence HUNGARIAN",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/Hungary/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence LANDING PAGE",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/Switching/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "GP Excellence ROMANIA",
+        path        : "//bybz25/default/main/MarketingSites/BSP/ExcellenceDesk/Romania/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Kiltix",
+        path        : "//bybz25/default/main/MarketingSites/AH/Kiltix/Hungary/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "MIRENA NZ",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Mirena/NewZealand/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "MS Diagnose",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Betaferon/Switzerland/MSTherapie/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "NZ Microsite 150years",
+        path        : "//bybz25/default/main/CorpSites/BAG/NewZealand/LifeBayer/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "OC Folate GLOBAL",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Yaz/International/ContraceptionChoices/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "OC Folate LATAM",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Yaz/LatinAmerica/ContraceptionChoices/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Ocho",
+        path        : "//bybz25/default/main/MarketingSites/BSP/EYLEA/Slovakia/Ochoreniemakuly/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Partnerprogram.bayer.com",
+        path        : "//bybz25/default/main/MarketingSites/AH/AnimalHealthProducts/Hungary/Partnerprogram/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Pille auf Reisen",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Contraception/Switzerland/Information/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Pretuval.ch",
+        path        : "//bybz25/default/main/MarketingSites/CC/Pretuval/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Qlaira",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Qlaira/Toolbox/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "SC Playground",
+        path        : "//bybz25/default/main/Playground/SoftcontrolPlayground/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Vivanza.ch",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Vivanza/Switzerland/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    },
+    {
+        name        : "Yazplus GLOBAL",
+        path        : "//bybz25/default/main/MarketingSites/BSP/Yaz/YazPlusBranded/WORKAREA/htdocs/",
+        keywords    : "",
+        wwwDev      : "",
+        wwwTest     : "",
+        wwwProd     : ""
+    }
+];
